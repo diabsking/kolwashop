eefbd8b3e586a7802254a443be20a995b685b215
diabsking <115880358+diabsking@users.noreply.github.com>
2025-03-02 00:34:30 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -27,10 +27,7 @@ app.use(session({
 }));
 
 // Configuration de Mongoose
-mongoose.connect('mongodb://mean-plants-fold.loca.lt:27017/kolwazshop', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect('mongodb://mean-plants-fold.loca.lt:27017/kolwazshop')
 .then(() => console.log("Connexion réussie à MongoDB en local"))
 .catch((err) => console.log("Erreur de connexion à MongoDB:", err));
 
